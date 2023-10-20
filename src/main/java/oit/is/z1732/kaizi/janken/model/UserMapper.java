7eeefd540f8553b22e98b12d73a63aa21bc31f6e
e1b21073 <e1b21073@oit.ac.jp>
2023-10-20 10:32:39 +0900

    ユーザ情報をSELECTで取得して表示

---
+++ src/main/java/oit/is/z1732/kaizi/janken/model/UserMapper.java
@@ -0,0 +1,15 @@
+package oit.is.z1732.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+//import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+  @Select("SELECT name from users;")
+  ArrayList<String> selectAllUsername();
+}
