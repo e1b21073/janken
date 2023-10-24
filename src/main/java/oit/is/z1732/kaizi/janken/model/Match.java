0ecbcbe054b0f67b9bc53251fd298a43b89aa21d
e1b21073 <e1b21073@oit.ac.jp>
2023-10-24 11:31:45 +0900

    試合を行い結果をデータベースに登録する処理の実装

--- src/main/java/oit/is/z1732/kaizi/janken/model/Match.java
+++ src/main/java/oit/is/z1732/kaizi/janken/model/Match.java
@@ -2,8 +2,8 @@ package oit.is.z1732.kaizi.janken.model;
 
 public class Match {
   int id;
-  String user1;
-  String user2;
+  int user1;
+  int user2;
   String user1Hand;
   String user2Hand;
 
@@ -15,19 +15,19 @@ public class Match {
     this.id = id;
   }
 
-  public String getUser1() {
+  public int getUser1() {
     return user1;
   }
 
-  public void setUser1(String user1) {
+  public void setUser1(int user1) {
     this.user1 = user1;
   }
 
-  public String getUser2() {
+  public int getUser2() {
     return user2;
   }
 
-  public void setUser2(String user2) {
+  public void setUser2(int user2) {
     this.user2 = user2;
   }
 
@@ -42,8 +42,9 @@ public class Match {
   public String getUser2Hand() {
     return user2Hand;
   }
-
+  
   public void setUser2Hand(String user2Hand) {
     this.user2Hand = user2Hand;
   }
+
 }
--- src/main/java/oit/is/z1732/kaizi/janken/model/UserMapper.java
+++ src/main/java/oit/is/z1732/kaizi/janken/model/UserMapper.java
@@ -15,4 +15,7 @@ public interface UserMapper {
 
   @Select("SELECT * from users where id = #{id}")
   User selectUserById(int id);
+
+  @Select("SELECT * from users where name = #{name}")
+  User selectUserByName(String name);
 }
