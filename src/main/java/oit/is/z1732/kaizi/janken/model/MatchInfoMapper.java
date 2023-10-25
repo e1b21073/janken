c7b44643d970c5b30d841902f53e32f6d5e2a43b
e1b21073 <e1b21073@oit.ac.jp>
2023-10-25 10:23:47 +0900

    ログイン後にアクティブな試合を表示

--- src/main/java/oit/is/z1732/kaizi/janken/model/MatchInfoMapper.java
+++ src/main/java/oit/is/z1732/kaizi/janken/model/MatchInfoMapper.java
@@ -1,12 +1,18 @@
 package oit.is.z1732.kaizi.janken.model;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface MatchInfoMapper {
   @Insert("Insert INTO matchinfo (user1,user2,user1Hand,isActive) VALUES (#{user1},#{user2},#{user1Hand},true);")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertMatchInfo(MatchInfo matchInfo);
+
+  @Select("SELECT * from matchinfo where isActive = true")
+  ArrayList<MatchInfo> selectActiveMatch();
 }
